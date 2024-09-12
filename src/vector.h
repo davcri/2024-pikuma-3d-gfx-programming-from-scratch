1bb36c1920a6be2cfb2c0023513e1e669e2ec100
Davide Cristini <davcri91@gmail.com>
2024-09-12 22:55:57 +0200

    feat: vector rotation

--- src/vector.h
+++ src/vector.h
@@ -21,4 +21,8 @@ typedef struct
     float fov;
 } camera_t;
 
+vec3_t vec3_rotate_x(vec3_t v, float angle);
+vec3_t vec3_rotate_y(vec3_t v, float angle);
+vec3_t vec3_rotate_z(vec3_t v, float angle);
+
 #endif // VECTOR2_H
